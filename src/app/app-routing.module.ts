bf094e670ba6731706db27afe22d0a3dcea23c62
Abid Zaidi <abidzaidi7@hotmail.com>
2020-10-31 18:46:10 +0500

    Adding story component and styling fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { JeesapComponent } from './jeesap/jeesap.component';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PaperplaneComponent } from './paperplane/paperplane.component';
-import { SlidingComponent } from './sliding/sliding.component';
+import { Story1Component } from './story1/story1.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'jeesap', component: JeesapComponent },
   { path: 'paperplane', component: PaperplaneComponent },
-  { path: 'sliding', component: SlidingComponent },
+  { path: 'story1', component: Story1Component },
   { path: '**', component: HomepageComponent },
 ];
 
---
+++ src/app/story1/story1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story1',
+  templateUrl: './story1.component.html',
+  styleUrls: ['./story1.component.scss']
+})
+export class Story1Component implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
